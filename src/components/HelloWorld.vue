f449f4d0870cc8a0d019c4b3dcc5e091ef073a70
masjedi2022 <masjediqaderi@gmail.com>
2025-09-27 16:14:00 +0430

    component changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,8 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+     
+      <h1>I am changed</h1>
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
